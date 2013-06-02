92b55d5ffb1627a0319d80edf212e2bb1984dd0b
Hassan Abdel-Rahman <hassan.abdel-rahman@flatironssolutions.com>
2013-06-02 10:56:36 -0400

    Added more graphs and carosel

--- js/app.js
+++ js/app.js
@@ -33,9 +33,6 @@ var edesignlabs = edesignlabs || {};
                 return chart;
             });
 
-
-
-
             /**************************************
              * Simple test data generator
              */
@@ -65,13 +62,147 @@ var edesignlabs = edesignlabs || {};
             }
         },
 
+        buildPieChart : function () {
+
+            var $chartWrapper = $('#piechart').find('.chart-wrapper'),
+                testdata = [
+                    {
+                        key: "One",
+                        y: 5
+                    },
+                    {
+                        key: "Two",
+                        y: 2
+                    },
+                    {
+                        key: "Three",
+                        y: 9
+                    },
+                    {
+                        key: "Four",
+                        y: 7
+                    },
+                    {
+                        key: "Five",
+                        y: 4
+                    },
+                    {
+                        key: "Six",
+                        y: 3
+                    },
+                    {
+                        key: "Seven",
+                        y: .5
+                    }
+                ];
+            $chartWrapper.empty();
+            $chartWrapper.append('<svg id="pie-chart-svg"/>');
+
+            nv.addGraph(function() {
+                var width = 500,
+                    height = 500;
+
+                var chart = nv.models.pieChart()
+                    .x(function(d) { return d.key })
+                    .y(function(d) { return d.y })
+                    //.showLabels(false)
+                    .values(function(d) { return d })
+                    .color(d3.scale.category10().range())
+                    .width(width)
+                    .height(height);
+
+                d3.select("#pie-chart-svg")
+                    .datum([testdata])
+                    .transition().duration(1200)
+                    .attr('width', width)
+                    .attr('height', height)
+                    .call(chart);
+
+                chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
+
+                return chart;
+            });
+        },
+
+        buildBarChart : function () {
+            var $chartWrapper = $('#barchart').find('.chart-wrapper'),
+
+                historicalBarChart = [
+                    {
+                        key: "Cumulative Return",
+                        values: [
+                            {
+                                "label": "A",
+                                "value": 29.765957771107
+                            } ,
+                            {
+                                "label": "B",
+                                "value": 0
+                            } ,
+                            {
+                                "label": "C",
+                                "value": 32.807804682612
+                            } ,
+                            {
+                                "label": "D",
+                                "value": 196.45946739256
+                            } ,
+                            {
+                                "label": "E",
+                                "value": 0.19434030906893
+                            } ,
+                            {
+                                "label": "F",
+                                "value": 98.079782601442
+                            } ,
+                            {
+                                "label": "G",
+                                "value": 13.925743130903
+                            } ,
+                            {
+                                "label": "H",
+                                "value": 5.1387322875705
+                            }
+                        ]
+                    }
+                ];
+
+            $chartWrapper.empty();
+            $chartWrapper.append('<svg id="bar-chart-svg"/>');
+
+
+            nv.addGraph(function() {
+                var chart = nv.models.discreteBarChart()
+                    .x(function(d) { return d.label })
+                    .y(function(d) { return d.value })
+                    .staggerLabels(true)
+                    //.staggerLabels(historicalBarChart[0].values.length > 8)
+                    .tooltips(false)
+                    .showValues(true)
+
+                d3.select('#bar-chart-svg')
+                    .datum(historicalBarChart)
+                    .transition().duration(500)
+                    .call(chart);
+
+                nv.utils.windowResize(chart.update);
+
+                return chart;
+            });
+        },
+
         wireUpReportTabEvents : function() {
             var that = this;
             $('.scatter-tab').on('shown', function (e) {
-                e.target // activated tab
-                e.relatedTarget // previous tab
                 that.buildScatterGraph();
             });
+            $('.pie-tab').on('shown', function (e) {
+                that.buildPieChart();
+            });
+            $('.bar-tab').on('shown', function (e) {
+                that.buildBarChart();
+            });
+
             $('.gallery-tab').on('shown', function (e) {
                 e.target // activated tab
                 e.relatedTarget // previous tab
@@ -108,7 +239,6 @@ var edesignlabs = edesignlabs || {};
             $('.nav-section').hide();
             showNavTab('#landing');
 
-            //this.buildScatterGraph();
             this.wireUpReportTabEvents();
         }
 

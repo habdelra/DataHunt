53eed1de4748c4203f1b10be0f33e53f7d6d8cde
Hassan Abdel-Rahman <hassan.abdel-rahman@flatironssolutions.com>
2013-06-02 09:24:07 -0400

    Bootstrappy stuff

--- js/app.js
+++ js/app.js
@@ -10,8 +10,26 @@ var edesignlabs = edesignlabs || {};
 
         //Bootstrap the app
         startApp: function () {
+            var showNavTab = function (tab) {
+                if (!tab) return;
+                var $selectedTab = $(tab);
+                if (!$selectedTab.is(':visible')) {
+                    $('.nav-sction').hide();
+                    $selectedTab.show();
+                }
+            };
 
+            //wire up nav tabs
+            $('.nav-tab').click(function(evt){
+                var $this = $(this),
+                    selectedTab = $this.attr('href');
+                showNavTab(selectedTab);
+
+            });
+            $('.nav-sction').hide();
+            showNavTab('#report');
         }
+
 
     };
 })(edesignlabs);
